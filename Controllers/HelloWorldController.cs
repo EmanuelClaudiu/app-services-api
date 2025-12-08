15f1c1f5f34b7b79265b1bbc899ab008410f74b6
Emanuel Ignat <EIgnat@boylesports.com>
2025-12-08 17:48:38 +0200

    Source code v1

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloWorldController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Helloo World!!";
+        }
+    }
+}
